e66944c502f2f7aebc741bcd0b09cbe8b059c32c
VovaNer <VovaNer@users.noreply.github.com>
2020-10-09 21:31:03 +0400

    Slider without anim

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,9 @@ const menuOpen = document.querySelector('.menu-open'),
     slidesWrapper = document.querySelector('.slider'),
     slidesInner = document.querySelector('.slider__inner'),
     slides = document.querySelectorAll('.slider__box'),
-    width = window.getComputedStyle(slidesWrapper).width,
+    sliderWidth = slidesWrapper.offsetWidth,
     sliderNext = document.querySelector('.slider__next'),
     sliderPrev = document.querySelector('.slider__prev');
-
 
 menuOpen.addEventListener('click', () => {
     menuBlock.classList.add('menu-show');
@@ -43,3 +42,99 @@ menuHide.addEventListener('click', () => {
 
 //     slidesInner.style.transform = `translateX(-${offset}px)`;
 // });
+
+let slideItems = 0;
+let slidesMargin = 20;
+let slideCounter = 0;
+let prevSlides = 0;
+
+const responsive = [
+    {breakPoint:{width: 0, item: 1}},
+    {breakPoint:{width: 575, item: 2}},
+    {breakPoint:{width: 767, item: 3}},
+    {breakPoint:{width: 992, item: 4}}
+];
+
+function load() {
+    for (let i = 0; i < responsive.length; i++) {
+        if (window.innerWidth > responsive[i].breakPoint.width) {
+            slideItems = responsive[i].breakPoint.item;
+        }
+    }
+
+    start();
+    formSlides();
+    
+}
+
+function formSlides(start = 0, end = slideItems) {
+    for (let i = start; i < end; i++) {
+        slides[i].classList.remove('hide');
+        slides[i].classList.add('show');
+        slideCounter++;
+    }
+}
+
+function start() {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.width = (sliderWidth - (slidesMargin * (slideItems-1))) / slideItems + 'px';
+        console.log(sliderWidth);
+    }
+    
+}
+
+function slideNext() {
+    if (slideCounter == slides.length) {
+        slideCounter = 0;
+        prevSlides = 0;
+        slides.forEach(slide => {
+            slide.classList.add('hide');
+            slide.classList.remove('show');
+        });
+        formSlides();
+    } else {
+        for (let i = 1; i < slides.length; i++) {
+            slides[i].style.margin = '0';
+        }
+        slides[prevSlides].classList.add('hide');
+        slides[prevSlides].classList.remove('show');
+        slides[slideCounter].classList.add('show');
+        slides[slideCounter].classList.remove('hide');
+        console.log(slideCounter);
+        console.log(prevSlides);
+        slideCounter++;
+        prevSlides++;
+    }
+}
+
+function slidePrev() {
+    if (prevSlides == 0) {
+        slides.forEach(slide => {
+            slide.classList.add('hide');
+            slide.classList.remove('show');
+        });
+        formSlides(slides.length-slideItems, slides.length);
+        slideCounter = slides.length;
+        prevSlides = slides.length - slideItems;
+        console.log(slideCounter);
+        console.log(prevSlides);
+    } else {
+        for (let i = 1; i < slides.length; i++) {
+            slides[i].style.margin = '0';
+        }
+        slides[slideCounter-1].classList.add('hide');
+        slides[slideCounter-1].classList.remove('show');
+        slides[prevSlides - 1].classList.add('show');
+        slides[prevSlides - 1].classList.remove('hide');
+        console.log(slideCounter);
+        console.log(prevSlides);
+        slideCounter--;
+        prevSlides--;
+    }
+}
+
+
+
+document.addEventListener('DOMContentLoaded', load);
+sliderNext.addEventListener('click', slideNext);
+sliderPrev.addEventListener('click', slidePrev);
